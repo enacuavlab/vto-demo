da08fcf90d666a6cb8944cd39fc70442b62c21f0
amtcvx <xp31@free.fr>
2023-02-16 10:42:35 +0100

    suppress old files

--- paparazzi_conf/restore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# copy all files from custom conf to paparazzi
-for f in $(find files/ -type f -printf '%P\n')
-do
-  mkdir -p $(dirname "$PAPARAZZI_HOME/$f")
-  cp "files/$f" "$PAPARAZZI_HOME/$f"
-done
